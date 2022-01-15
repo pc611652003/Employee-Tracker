d33b89ae63924ab40f36f3180f353afe860912eb
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2022-01-15 04:22:46 -0700

    Added Basic Functionality

--- index.js
+++ index.js
@@ -21,43 +21,46 @@ const init = () => {
             message: 'What would you like to do?',
             choices: ['View all departments', 'View all roles', 'View all employees', 
             'Add a department', 'Add a role', 'Add an employee', 
-            'Update an employee role', 'Exit']
+            'Update an employee role', 'Update an employee manager',
+            'View employees by manager', 'View employees by department',
+            'Delete an employee', 'Delete a role', 'Delete a department',
+            'View total utilized budget', 'Exit']
         })
         .then(answer => {
             switch (answer.actionChoice) {
                 case 'View all departments':
-                    department.viewAllDepartment()
-                    .then(() => init());
-                    return;                
+                    return department.viewAllDepartment();                
                 case 'View all roles':
-                    role.viewAllRole()
-                    .then(() => init());
-                    return;
+                    return role.viewAllRole();
                 case 'View all employees':
-                    employee.viewAllEmployee()
-                    .then(() => init());
-                    return;
+                    return employee.viewAllEmployee();
                 case 'Add a department':
-                    department.addDepartment()
-                    .then(() => init()); 
-                    return;
+                    return department.addDepartment();
                 case 'Add a role':
-                    department.getDepartmentList()
-                    .then((list) => {
-                        role.addRole(list)
-                        .then(() => init());
-                    })
-                    return;
+                    return role.addRole();
                 case 'Add an employee':
-                    return;
+                    return employee.addEmployee();
                 case 'Update an employee role':
+                    return employee.updateEmployeeRole();
+                case 'Update an employee manager':
+                    return employee.updateEmployeeManager();
+                case 'View employees by manager':
+                    return employee.viewEmployeeByManager(); 
+                case 'View employees by department':
+                    return employee.viewEmployeeByDepartment();
+                case 'Delete an employee':
+                    return employee.deleteEmployee();
+                case 'Delete a role':
                     return;
                 case 'Delete a department':
+                    return;
+                case 'View total utilized budget':
                     return;
                 case 'Exit':
                     process.exit();
             }
         })
+        .then(() => init());
 
 };
 //DBconnect();
